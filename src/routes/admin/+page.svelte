b6c35ea89380dd26e3b4c4eacaa916398ab70709
apb718 <apb718@gmail.com>
2024-11-26 22:10:14 -0600

    add admin login page (no function)

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,28 @@
+<div class='row py-5'>
+    <div class='col-xl-4'></div>
+    <div class='card p-4 shadow col-xl-4'>
+        <div class="text-center mb-4">
+            <h2>Login</h2>
+        </div>
+        <form>
+        <!-- Email Input -->
+        <div class="mb-3">
+          <label for="email" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="email" placeholder="Enter your email" required>
+        </div>
+        <!-- Password Input -->
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input type="password" class="form-control" id="password" placeholder="Enter your password" required>
+        </div>
+        <!-- Remember Me Checkbox -->
+        <div class="form-check mb-3">
+          <input type="checkbox" class="form-check-input" id="rememberMe">
+          <label class="form-check-label" for="rememberMe">Remember Me</label>
+        </div>
+        <!-- Submit Button -->
+        <button type="submit" class="btn btn-primary w-100">Login</button>
+      </form>
+    </div>
+    <div class='col-xl-4'></div>
+</div>
