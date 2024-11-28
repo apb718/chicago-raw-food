c22222893d1695d8182544327550d5dffbcefee4
apb718 <apb718@gmail.com>
2024-11-28 01:03:22 -0600

    polly banner

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,16 @@
 <script>
   import AboutBanner from '$lib/about/AboutBanner.svelte';
+  import OurStory from '$lib/about/OurStory.svelte';
+  import MargoBanner from "$lib/about/MargoBanner.svelte";
+  import PollyBanner from "$lib/about/PollyBanner.svelte";
+  import WhatIsRaw from "$lib/about/WhatIsRaw.svelte";
 </script>
 <head>
   <title>About Us</title>
 </head>
 
 <AboutBanner />
+<OurStory />
+<PollyBanner />
+<WhatIsRaw />
+<MargoBanner />
