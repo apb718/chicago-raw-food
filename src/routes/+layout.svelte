38d889b5d16f1897ba1434fcb03ee98f2e5ca42b
apb718 <apb718@gmail.com>
2024-11-28 21:23:24 -0600

    Implements the weekly takeaway info page
    Signed-off-by: apb718 <apb718@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,17 @@
 <script>
   import {browser} from '$app/environment'; // Detect if running in the browser
   import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-  import 'bootstrap-icons/font/bootstrap-icons.css'
+  import 'bootstrap-icons/font/bootstrap-icons.css';
   import Navbar from '$lib/Navbar.svelte';
   import Footer from '$lib/Footer.svelte';
+
+  import('@fortawesome/fontawesome-free/css/all.css');
 
   // Dynamically import Bootstrap JavaScript only in the browser
   if (browser) {
     // @ts-ignore
     import('bootstrap/dist/js/bootstrap.bundle.js');
+
   }
 </script>
 
