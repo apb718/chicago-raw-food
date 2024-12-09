926f68b5039f469d8cd822bda640bfbdf8f9fe4e
apb718 <apb718@gmail.com>
2024-12-09 02:00:23 -0500

    set up auth

---
+++ chicago-raw-food-app/src/hooks.server.ts
@@ -0,0 +1,49 @@
+import { pool } from "$lib/db/mysql.ts";
+import { redirect, type Handle } from "@sveltejs/kit";
+import { updateAuth } from "$lib/server/updateAuth.js";
+
+export const handle: Handle = async ({ event, resolve }) => {
+    const isAuth: boolean = event.url.pathname === '/auth';
+
+    if (isAuth) {
+        event.cookies.set('auth_token', '', {
+            path: '/',
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            maxAge: 0
+        });
+        return resolve(event);
+    }
+
+    const auth_cookie = event.cookies.get('auth_token') ?? '';
+    console.log(auth_cookie)
+    const token = await pool
+        .query("SELECT UUID, expiry_time FROM Auth WHERE UUID=?",
+                 [auth_cookie]);
+
+    // @ts-ignore
+
+    try {
+        // @ts-ignore
+        const user = token[0][0];
+        const user_uuid = user.UUID;
+        const user_expiry = user.expiry_time;
+
+        if(new Date().getTime() < user_expiry) {
+            await updateAuth(user_uuid);
+        } else {
+            // Token has gone dead
+        }
+    } catch (error) {
+        console.log("Unable to reauthenticate UUID");
+    }
+
+
+
+
+
+
+    const response = await resolve(event);
+    return response;
+};
--- chicago-raw-food-app/src/routes/auth/+page.svelte
+++ chicago-raw-food-app/src/routes/auth/+page.svelte
@@ -1,22 +1,67 @@
-<script lang=”ts”>
-    import { pool } from '$lib/db/mysql.ts'
+<script lang="ts">
+    let email = '';
+    let password = '';
+    let error: string | null = null;
 
-    const ms = pool;
-
-    async function login(form) {
+    async function login(): Promise<void> {
+        error = null;
         try {
-            await ms.query("SELECT * ")
+            const response = await fetch('/api/v1/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password })
+            });
+            // console.log('Login');
+
+            if (!response.ok) {
+                const { error } = await response.json();
+                throw new Error(error);
+            } else {
+                window.location.href = '/admin';
+            }
+
         } catch (err) {
-            console.error(err);
+
         }
     }
 </script>
 
-<form method="post" on:submit|preventDefault={(e) => login(e.currentTarget)}>
-    <input name="token" type="hidden" />
-    <button
-            class="border rounded p-2 mt-10 bg-gray-800 text-white hover:bg-gray-700"
-    >
-        Login using Github
-    </button>
-</form>
+<div class='row py-5'>
+    <div class='col-xl-4'></div>
+    <div class='card p-4 shadow col-xl-4'>
+        <div class="text-center mb-4">
+            <h2>Login</h2>
+        </div>
+        <form on:submit|preventDefault={login}>
+            <!-- Email Input -->
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input
+                        type="email"
+                        class="form-control"
+                        id="email"
+                        placeholder="Enter your email"
+                        bind:value={email}
+                        required>
+            </div>
+            <!-- Password Input -->
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password"
+                       class="form-control"
+                       id="password"
+                       placeholder="Enter your password"
+                       bind:value={password}
+                       required>
+            </div>
+            <!-- Remember Me Checkbox -->
+            <div class="form-check mb-3">
+                <input type="checkbox" class="form-check-input" id="rememberMe">
+                <label class="form-check-label" for="rememberMe">Remember Me</label>
+            </div>
+            <!-- Submit Button -->
+            <button type="submit" class="btn btn-primary w-100">Login</button>
+        </form>
+    </div>
+    <div class='col-xl-4'></div>
+</div>
