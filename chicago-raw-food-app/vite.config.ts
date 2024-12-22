87a2f59dffb67f6d8d351ec6eacfeab07db83571
apb718 <apb718@gmail.com>
2024-12-21 22:13:35 -0600

    fix issues with deploy having missing dependency

--- chicago-raw-food-app/vite.config.ts
+++ chicago-raw-food-app/vite.config.ts
@@ -1,12 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { enhancedImages } from "@sveltejs/enhanced-img";
 import * as fs from "node:fs";
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		enhancedImages(),
 	],
 	server: {
 		https: {
