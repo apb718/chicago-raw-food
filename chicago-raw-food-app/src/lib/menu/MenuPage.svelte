222dd17ed2ea95e5a67ec95f343a0ffbdc73b054
apb718 <apb718@gmail.com>
2024-12-25 01:43:59 -0600

    load animation on menu

--- chicago-raw-food-app/src/lib/menu/MenuPage.svelte
+++ chicago-raw-food-app/src/lib/menu/MenuPage.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import { slide } from "svelte/transition"
     import { capitalizeWords } from "$lib/CapitalizeWords.js";
     import {removeTrailingZeroes} from "$lib/removeTrailingZeroes.js";
 
@@ -40,24 +41,39 @@
 </script>
 
 <!-- Render Products -->
-{#if isLoaded}
-    {#if products.length > 0}
-        <div>
+{#if isLoaded && products.length > 0}
 
-            {#each products as product (product.product_id)}
-                {#if !displayedTypes.has(product.type_description)}
-                    <h1 class="center-text text-strong-pink">{product.type_description}</h1>
-                    {@html (() => { displayedTypes.add(product.type_description); })()}
-                {/if}
+    <section
+            class="animated-section"
+            in:slide={{ duration: 400 }}
+    >
 
-                <div>
-                    <p class="center-text text-strong-green">{capitalizeWords(product.product_name)}</p>
-                    <p class="center-text" style="margin-top: -15px">{removeTrailingZeroes(product.price)}</p>
-                    <p class="center-text" style="margin-top: -15px">{product.description}</p>
-                </div>
-            {/each}
-        </div>
-    {:else}
-        <p>Loading or no products available...</p>
-    {/if}
+        {#each products as product (product.product_id)}
+            {#if !displayedTypes.has(product.type_description)}
+                <h1 class="center-text text-strong-pink">{product.type_description}</h1>
+                {@html (() => { displayedTypes.add(product.type_description); })()}
+            {/if}
+
+            <div class="mb-5">
+                <p class="center-text text-strong-green">{capitalizeWords(product.product_name)}</p>
+                <p class="center-text" style="margin-top: -15px">{removeTrailingZeroes(product.price)}</p>
+                <p class="center-text" style="margin-top: -15px">{product.description}</p>
+            </div>
+        {/each}
+    </section>
+{:else}
+    <p>Loading or no products available...</p>
 {/if}
+
+
+<style>
+    /*section.animated-section {*/
+    /*    margin: 2rem auto;*/
+    /*    padding: 1rem;*/
+    /*    max-width: 600px;*/
+    /*    background-color: #f9f9f9;*/
+    /*    border: 1px solid #ddd;*/
+    /*    border-radius: 8px;*/
+    /*    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);*/
+    /*}*/
+</style>
