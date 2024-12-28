a8ad54626711fb9c0ffe81c25565dd1d3a0476f8
apb718 <apb718@gmail.com>
2024-12-27 22:57:09 -0600

    fix hooks

--- chicago-raw-food-app/src/hooks.server.ts
+++ chicago-raw-food-app/src/hooks.server.ts
@@ -1,6 +1,4 @@
-// import { pool } from "$lib/db/mysql.ts";
-import { type Handle } from "@sveltejs/kit";
-import { updateAuth } from "$lib/server/updateAuth.js";
+import {type Handle} from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
     const isAuth: boolean = event.url.pathname === '/auth';
@@ -15,29 +13,6 @@ export const handle: Handle = async ({ event, resolve }) => {
         });
         return resolve(event);
     }
-    //
-    // const auth_cookie = event.cookies.get('auth_token') ?? '';
-    // console.log(auth_cookie)
-    // const token = await pool
-    //     .query("SELECT UUID, expiry_time FROM Auth WHERE UUID=?",
-    //              [auth_cookie]);
-    //
-    // // @ts-ignore
-    //
-    // try {
-    //     // @ts-ignore
-    //     const user = token[0][0];
-    //     const user_uuid = user.UUID;
-    //     const user_expiry = user.expiry_time;
-    //
-    //     if(new Date().getTime() < user_expiry) {
-    //         await updateAuth(user_uuid);
-    //     } else {
-    //         // Token has gone dead
-    //     }
-    // } catch (error) {
-    //     console.log("Unable to reauthenticate UUID");
-    // }
 
     const response = await resolve(event);
     return response;
