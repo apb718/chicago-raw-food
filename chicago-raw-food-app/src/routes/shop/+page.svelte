0d862f21b65200a8dccbf751c80b61f46594ed45
apb718 <apb718@gmail.com>
2024-12-30 22:34:22 -0600

    adds remaining changes

--- chicago-raw-food-app/src/routes/shop/+page.svelte
+++ chicago-raw-food-app/src/routes/shop/+page.svelte
@@ -9,15 +9,20 @@
 
     let categoryId: number = DEHYDRATED_ID;
 
-    let products = $state<Product[]>();
-    let cartProducts = $state<CartProduct[]>([]);
+    let products: Product[] = $state<Product[]>();
+    let cartProducts: CartProduct[] = $state<CartProduct[]>([]);
+
+    let loading = $state(true);
+
 
     async function load() {
+
         const apiUrl = "/api/v1/menu/";
         const response = await fetch(`${apiUrl}${categoryId}`);
         const categoryProducts = await response.json();
 
         products = [...categoryProducts];
+        loading = false
     }
 
     onMount(() => {
@@ -37,43 +42,48 @@
         text="Shop"
 />
 
-
+{#if !loading && products.length > 0}
 <div class="row">
     <div class="col-xl-3"></div>
     <div class="col-xl-6">
         <div class="row">
-            {#each products as product}
-                {#if product.active}
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card h-100">
-                            <!--{console.log(product)}-->
-                            <img
-                                    src="{product.image_url}"
-                                    class="card-img-top"
-                                    alt={capitalizeWords(product.product_name)}
-                            />
-                            <div class="card-body">
-                                <h5 class="card-title">{capitalizeWords(product.product_name)}</h5>
-                                <p class="card-text">{product.description}</p>
-                                <p class="card-text"><strong>${product.price}</strong></p>
-                                <button
-                                        class="rounded hover:blue"
-                                        onclick={() => {
-                                            return cartProducts.push({
-                                                id: crypto.randomUUID(),
-                                                quantity: 1,
-                                                product: pioduct
-                                            });
-                                        }}
-                                >
-                                    Button
-                                </button>
+
+                {#each products as product}
+                    {#if product.active}
+                        <div class="col-lg-4 mb-4">
+                            <div class="card h-100">
+                                <!--{console.log(product)}-->
+                                <img
+                                        src="{product.image_url}"
+                                        class="card-img-top"
+                                        alt={capitalizeWords(product.product_name)}
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">{capitalizeWords(product.product_name)}</h5>
+                                    <p class="card-text">{product.description}</p>
+                                    <p class="card-text"><strong>${product.price}</strong></p>
+                                    <button
+                                            class="rounded hover:blue"
+                                            onclick={() => {
+                                                console.log(`cart: ${JSON.stringify(cartProducts)}`);
+                                                return cartProducts.push({
+                                                    id: crypto.randomUUID(),
+                                                    quantity: 1,
+                                                    product: product
+                                                });
+
+                                            }}
+                                    >
+                                        Button
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </div>                {/if}
+                        </div>                {/if}
 
 
-            {/each}
+                {/each}
+
+            </div>
         </div>
     </div>
-</div>
+{/if}
